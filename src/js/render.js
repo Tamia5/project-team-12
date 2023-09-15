cd04aa41d33383f32c5d7a9084bfdca6eef3d0c2
19Aleksandra95 <polar.heder@gmail.com>
2023-09-15 18:02:04 +0200

    add change ah

--- src/js/render.js
+++ src/js/render.js
@@ -5,20 +5,20 @@
 function markup(data) {
     const CardMarkup = data => {
       const { cook, topic } = data;
-      return `<div class="swiper-slide">
-    <div class="slider-card">
-      <div class="chief-cook" style="background-image: url(${cook.imgUrl})">
+      return `<div class=“swiper-slide”>
+    <div class=“slider-card”>
+      <div class=“chief-cook” style=“background-image: url(${cook.imgUrl})“>
       </div>
-      <div class="mini-picture-card">
-        <div class="mini-picture" style="background-image: url(${topic.previewWebpUrl})"></div>
-        <p class="dish-name">
+      <div class=“mini-picture-card”>
+        <div class=“mini-picture” style=“background-image: url(${topic.previewWebpUrl})“></div>
+        <p class=“dish-name”>
           ${topic.name}
         </p>
-        <p class="country">
+        <p class=“country”>
           ${topic.area}
         </p>
       </div>
-      <div class="large-picture" style="background-image: url(${topic.previewUrl})">
+      <div class=“large-picture” style=“background-image: url(${topic.previewUrl})“>
       </div>
     </div>
   </div>`;
