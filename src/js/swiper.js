247c929882ca95571d01f3ca79553faabbea480d
19Aleksandra95 <polar.heder@gmail.com>
2023-09-16 15:58:44 +0200

    commit

---
+++ src/js/swiper.js
@@ -0,0 +1,31 @@
+
+import Swiper from 'swiper';
+import { Pagination, Navigation, Autoplay, Parallax } from 'swiper/modules';
+// import 'swiper/swiper-bundle.min.css';
+
+const sliderContainer = document.querySelector('.events');
+
+function events() {
+  fetch(`https://tasty-treats-backend.p.goit.global/api/events`)
+    .then(res => res.json())
+    .then(res => {
+       markup(res);
+      const swiper = new Swiper('.swiper', {
+        modules: [Pagination, Navigation, Autoplay, Parallax],
+        allowSlideNext: true,
+        pagination: {
+          el: '.slider-pagination',
+          clickable: true,
+        },
+        autoplay: {
+          delay: 2500,
+        },
+        parallax: true,
+        speed: 1000,
+    
+        loop: true,
+      });
+    }).catch(error => console.error('Error:', error)) 
+}
+events();
+
