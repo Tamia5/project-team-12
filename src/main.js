aef02e28a9faf17781bf6a6b9519cfffc7f25edc
p0daru <102858022+p0daru@users.noreply.github.com>
2023-09-16 20:43:59 +0300

    update main.js
    import './js/popular-recipes';

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import './js/swiper';
-import './js/API'
-import './js/render'
-import './js/header-btn-menu'
-import './js/categories-btn'
-import './js/search-recipes'
-
-
+import './js/API';
+import './js/render';
+import './js/header-btn-menu';
+import './js/categories-btn';
+import './js/search-recipes';
+import './js/popular-recipes';
