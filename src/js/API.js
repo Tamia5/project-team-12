4b9e0771ade22f4bc4daffb172495307f81d9e4e
p0daru <102858022+p0daru@users.noreply.github.com>
2023-09-16 20:55:19 +0300

    update api-js
    adding getPopRecipes()

--- src/js/API.js
+++ src/js/API.js
@@ -1,53 +1,53 @@
 // Імпортувати сюди всі функції запитів
-const url = `https://tasty-treats-backend.p.goit.global/api/recipes`
+const url = `https://tasty-treats-backend.p.goit.global/api/recipes`;
 
-                                                // ЗАПИТ ВСІХ РЕЦЕПТІВ //
+// ЗАПИТ ВСІХ РЕЦЕПТІВ //
 function fetchRecipe(limit, page, category, time, area, ingredient) {
-    const params = new URLSearchParams({
-        limit: limit,
-        page: page,
-        category: category,
-        time: time,
-        area: area,
-        ingredient: ingredient
-   })
-    return fetch(`${url}?${params}`)
-        .then(resp => {
-            if (!resp.ok) {
-                console.log("Pizdez")
-            }
-            return resp.json()
-        })
+  const params = new URLSearchParams({
+    limit: limit,
+    page: page,
+    category: category,
+    time: time,
+    area: area,
+    ingredient: ingredient,
+  });
+  return fetch(`${url}?${params}`).then(resp => {
+    if (!resp.ok) {
+      console.log('Pizdez');
+    }
+    return resp.json();
+  });
 }
-export {fetchRecipe}
+export { fetchRecipe };
 
-                                                // ЗАПИТ ВСІХ КРАЇН //
+// ЗАПИТ ВСІХ КРАЇН //
 function fetchAreas() {
-    return fetch(`https://tasty-treats-backend.p.goit.global/api/areas`)
-        .then(resp => {
-            if (!resp.ok) {
-                console.log(`Err areas`)
-            }
-            return resp.json()
-        })
+  return fetch(`https://tasty-treats-backend.p.goit.global/api/areas`).then(
+    resp => {
+      if (!resp.ok) {
+        console.log(`Err areas`);
+      }
+      return resp.json();
+    }
+  );
 }
-export {fetchAreas}
+export { fetchAreas };
 
-                                                // ЗАПИТ ВСІХ ІНГРЕДІЄНТІВ //
+// ЗАПИТ ВСІХ ІНГРЕДІЄНТІВ //
 function fetchIngredients() {
-    return fetch(`https://tasty-treats-backend.p.goit.global/api/ingredients`)
-        .then(resp => {
-            if (!resp.ok) {
-                console.log(`Err ingredients`)
-            }
-            return resp.json()
-        })
+  return fetch(
+    `https://tasty-treats-backend.p.goit.global/api/ingredients`
+  ).then(resp => {
+    if (!resp.ok) {
+      console.log(`Err ingredients`);
+    }
+    return resp.json();
+  });
 }
-export {fetchIngredients}
-
+export { fetchIngredients };
 
 // Hero Swiper
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/events'
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/events';
 
 async function fetchMasterClass() {
   try {
@@ -70,5 +70,20 @@ fetchMasterClass()
   .catch(error => console.log(error));
 
 function createMarkupSwiper(arrSliders) {
-  const swiperWrapperEl = document.querySelector('.swiper-wrapper'); }
-  
+  const swiperWrapperEl = document.querySelector('.swiper-wrapper');
+}
+
+// ОТРИМАТИ ПОПУЛЯРНІ РЕЦЕПТИ //
+function getPopRecipes() {
+  return fetch(`${url}/popular`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      throw error;
+    });
+}
+export { getPopRecipes };
