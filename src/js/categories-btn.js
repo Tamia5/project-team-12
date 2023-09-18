46dd486db84bcc320d3c65c5c3b0c53c99bf4565
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-09-18 11:34:11 +0300

    add and remove class active

--- src/js/categories-btn.js
+++ src/js/categories-btn.js
@@ -1,43 +1,96 @@
+import { functions } from 'lodash';
 import { getCategoriesFromAPI } from './API.js';
 import { renderCategories } from './render.js';
 
 const categoriesList = document.querySelector('.js-categories');
 const allCategoriesButton = document.querySelector('.js-btn-all-categories');
-
-
-function addClickListenersToCategories(categories) {
-  const categoryItems = categoriesList.querySelectorAll('.js-categories-item');
-
-  categoryItems.forEach(item => {
-    item.addEventListener('click', () => {
-      const category = item.textContent;
-      getRecipesByCategory(category);
-
- 
-      categoryItems.forEach(categoryItem => {
-        categoryItem.classList.remove('active');
-      });
-
-   
-      item.classList.add('active');
-
-    
-      if (allCategoriesButton) {
-        allCategoriesButton.classList.remove('active-category');
-      }
-    });
-  });
-}
-
+const categoriesContainer = document.querySelector('.js-categories-container');
 
 async function loadCategories() {
   const categories = await getCategoriesFromAPI();
     renderCategories(categories, categoriesList); 
     
   if (allCategoriesButton) {
-    allCategoriesButton.classList.add('active-category');
+    allCategoriesButton.classList.add('active-btn');
   }
 }
 
 loadCategories();
+
+categoriesList.addEventListener('click', onSelectedCategories);
+
+
+
+function onSelectedCategories(evt) {
+  const isSelectedCategoryEl = evt.target.classList.contains('js-categories-item');
+  if (!isSelectedCategoryEl) {
+    return;
+  }
+  const selectedCategoryEl = evt.target.closest('.js-categories-item');
+  selectedCategoryEl.classList.add('active');
+  allCategoriesButton.classList.remove('active-btn');
+ 
+  removeActiveClassFromCategory(selectedCategoryEl);
+}
+
+function removeActiveClassFromCategory(clickedCategory) {
+
+  const categoryElements = document.querySelectorAll('.js-categories-item');
+
+ 
+  categoryElements.forEach(categoryEl => {
+    if (categoryEl !== clickedCategory) {
+      categoryEl.classList.remove('active');
+    }
+  });
+}
+
+
+
+categoriesContainer.addEventListener('click', onSelectedCategories);
+
+
+// categoriesContainer.addEventListener('click', onButtonCLick);
+
+// let lastClickedButton = null;
+// function onButtonCLick(event) {
+//   const button = event.target;
+
+//   if (button.nodeName !== 'BUTTON') {
+//     return;
+//   }
+
+//   if (lastClickedButton) {
+//     lastClickedButton.classList.remove('active');
+//   }
+
+//   if (button === allCategoriesButton) {
+//     removeActiveClassFromAllButtons();
+//   } else {
+//     allCategoriesButton.classList.remove('active');
+//   }
+
+//   if (button.classList.contains('js-categories-item')) {
+//     button.classList.add('active');
+//   }
+
+//   lastClickedButton = button;
+// }
+
+// function removeActiveClassFromAllButtons() {
+//   const buttons = categoriesList.querySelectorAll('button');
+
+//   buttons.forEach(button => {
+//     button.classList.remove('active');
+//   });
+// }
+
+// categoriesList.addEventListener('click', event => {
+//   if (!event.target.classList.contains('js-categories-item')) {
+//     event.stopPropagation();
+//   }
+// });
+
+
+
 
