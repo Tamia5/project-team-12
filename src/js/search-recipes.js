b9acd02e9464bf9ee456597a88afdb67e55cbdd8
HannaNikola <h.nikolaienko32@gmail.com>
2023-09-20 16:02:18 +0200

    add-pagination

--- src/js/search-recipes.js
+++ src/js/search-recipes.js
@@ -3,6 +3,12 @@ import { fetchAreas, fetchIngredients, fetchRecipe } from "./API"
 import lodash from 'lodash'
 
 const dobounce = lodash.debounce
+// добавила перемінні для пагінаціі
+const paginationContainer = document.querySelector('.js-pagination');
+
+let currentPage = 1;
+let totalPages = 1;
+
 
 const elements = {
     container: document.querySelector(`.categories-container`),
@@ -39,7 +45,13 @@ startRecipe()
 function startRecipe(evt) {
 fetchRecipe(limit, page, category, time, area, ingredient)
     .then(data => { 
-    elements.container.innerHTML = createMarkup(data.results)
+        // данні для пагінація 
+        totalPages = data.totalPages;
+        currentPage = data.currentPage;
+
+        elements.container.innerHTML = createMarkup(data.results)
+        // визов функціі для пагінаціі
+        updatePagination();
     })
     .catch(err => console.log(`err`))
 }
@@ -78,3 +90,62 @@ function resetRecipes(evt) {
     startRecipe()
 }   
 
+
+// пагінація
+
+
+
+function updatePagination() {
+  paginationContainer.innerHTML = '';
+
+  const pagesToShow = 3;
+
+  let startPage = Math.max(currentPage - Math.floor(pagesToShow / 2), 1);
+  let endPage = startPage + pagesToShow - 1;
+
+  if (endPage > totalPages) {
+    endPage = totalPages;
+    startPage = Math.max(endPage - pagesToShow + 1, 1);
+  }
+
+  if (currentPage > 1) {
+    paginationContainer.innerHTML += `<button class="js-first-page"><<</button>`;
+    paginationContainer.innerHTML += `<button class="js-previous-page"><</button>`;
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    if (i === currentPage) {
+      paginationContainer.innerHTML += `<button class="js-page active" data-page="${i}">${i}</button>`;
+    } else {
+      paginationContainer.innerHTML += `<button class="js-page" data-page="${i}">${i}</button>`;
+    }
+  }
+  if (currentPage <= totalPages - 3) {
+    paginationContainer.innerHTML += `<p class="js-three-dots">...</button>`;
+  }
+  if (currentPage < totalPages) {
+    paginationContainer.innerHTML += `<button class="js-next-page">></button>`;
+    paginationContainer.innerHTML += `<button class="js-last-page">>></button>`;
+  }
+}
+
+paginationContainer.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.classList.contains('js-first-page')) {
+    currentPage = 1;
+  } else if (target.classList.contains('js-previous-page')) {
+    currentPage = Math.max(currentPage - 1, 1);
+  } else if (target.classList.contains('js-next-page')) {
+    currentPage = Math.min(currentPage + 1, totalPages);
+  } else if (target.classList.contains('js-last-page')) {
+    currentPage = totalPages;
+  } else if (target.classList.contains('js-page')) {
+    currentPage = parseInt(target.getAttribute('data-page'));
+    }
+    
+  startRecipe(currentPage);
+});
+
+
+// startRecipe(currentPage);
